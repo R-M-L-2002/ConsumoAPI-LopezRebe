c962f39c020672d1c61053fb379bc33c45929a8d
Rebe <r.lopez@itecriocuarto.org.ar>
2025-03-31 18:00:18 -0300

    Upgrade en renderizado
    css mejorado con primeract

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,61 @@
-import { useState, useEffect, Fragment } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, Fragment } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import 'primereact/resources/themes/md-dark-deeppurple/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './App.css';
 
 function App() {
-  const [quotes, setQuotes] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setQuotes(data);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+    const fetchData = async () => {
+      try {
+        //usuarios
+        const users = await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+        //post y comentario
+        const userData = await Promise.all(
+          users.map(async (user) => {
+            const posts = await fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`).then(res => res.json());
+            const post = posts[0];
+
+            const comments = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`).then(res => res.json());
+            const comment = comments[0];
+
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              postTitle: post.title,
+              postBody: post.body,
+              comment: comment.body,
+            };
+          })
+        );
+
+        setData(userData);
+
+      } catch (error) {
+        console.error('Error API:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div className="container">
-      <h1>Lista de Posts</h1>
-      <div className="posts-container">
-        {quotes.map((quotes) => (
-          <div className="post-card" key={quotes.id}>
-            <h3>{quotes.title}</h3>
-            <p>{quotes.body}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Fragment className="container">
+      <DataTable value={data} paginator rows={5}> {/* paginacion re cheta */}
+        <Column field="name" header="Usuario"></Column>
+        <Column field="email" header="Email"></Column>
+        <Column field="postTitle" header="Titulo"></Column>
+        <Column field="postBody" header="Contenido"></Column>
+        <Column field="comment" header="Comentario"></Column>
+      </DataTable>
+    </Fragment>
   );
 }
 
-export default App
+export default App;
